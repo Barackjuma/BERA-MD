4d2e7d618846cc6da8a9f29c73c42c40478955f8
Barackjuma <jumabarack17@gmail.com>
2025-02-09 19:36:40 +0300

    Update Ownermiddleware.js

--- utility/botUtil/Ownermiddleware.js
+++ utility/botUtil/Ownermiddleware.js
@@ -4,7 +4,7 @@ const Ownermiddleware = async (context, next) => {
     const { m, Owner } = context;
 
     if (!Owner) {
-        return m.reply("You need owner privileges to execute this command.");
+        return m.reply("You need owner privileges to execute this command.Contact 🅑︎🅐︎🅡︎🅐︎🅒︎🅚︎ should you need any help.");
     }
 
     await next();
